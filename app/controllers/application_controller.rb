1dbc87ca2ee61e021f214a578cb41d626e3bbe38
jon-elofson <jonathan.elofson@gmail.com>
2015-09-11 19:47:41 -0700

    add registration controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,28 +5,13 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate_user!
 
-  # helper_method :current_user, :logged_in?, :ensure_logged_in
+  def after_sign_in_path_for(resource)
+    current_user_path
+  end
 
-  # def login!(user)
-  #   session[:session_token] = user.reset_session_token!
-  # end
-  #
-  # def logout!
-  #   session[:session_token] = nil;
-  # end
-  #
-  # def current_user
-  #   User.find_by_session_token(session[:session_token])
-  # end
-  #
-  # def logged_in?
-  #   !!current_user
-  # end
-  #
-  # def ensure_logged_in
-  #   return if logged_in?
-  #   redirect_to new_session_url
-  # end
+  def after_sign_up_path_for(resource)
+    current_user_path
+  end
 
 
 
